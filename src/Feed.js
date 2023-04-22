206af3c35e396e98645621f20eb8b4a9c96a090b
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2023-04-22 18:23:46 +0530

    final

---
+++ src/Feed.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import ArticleIcon from '@mui/icons-material/Article';
+import './css/feed.css'
+import { Avatar } from '@material-ui/core'
+
+function Feed() {
+    return (
+        <div className='feed'>
+            <div className='feed__input'>
+                <Avatar/>
+                <form> 
+                    <input type='text' placeholder='Start a post'/>
+                    <input type='submit'/>
+                </form>
+            </div>
+
+            <div className='feed__options'>
+                <div className='option'>
+                    <AddAPhotoIcon/>
+                    <span>Photo</span>
+                </div>
+                <div className='option'>
+                    <VideoCallIcon/>
+                    <span>Video</span>
+                </div>
+                <div className='option'>
+                    <EventAvailableIcon/>
+                    <span>Event</span>
+                </div>
+                <div className='option'>
+                    <ArticleIcon/>
+                    <span>Article</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Feed
